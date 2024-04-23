449c73c4ac3c5aec8c78f54cb9dff9dda59093be
Suresh Sundararajan <yessures@gmail.com>
2024-04-22 19:28:18 -0600

    Controller, node and postgres DB with working configuration. renamed index.html as base.html and viseversa.

---
+++ docker-app/dc_up.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+docker-compose build 
+docker-compose up -d
+sleep 5
+docker ps 
+CON_ID=$(docker ps|awk '{print $1}'|grep -iv con)
+docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}} %tab% {{.Name}}' $CON_ID 
---
+++ docker-app/demo/set_profile.sh
@@ -0,0 +1,2 @@
+echo 'PATH=/root/.local/bin:$PATH; export PATH' >> /root/.profile
+echo 'ANSIBLE_CONFIG=/app/ansible.cfg; export ANSIBLE_CONFIG' >> /root/.profile
